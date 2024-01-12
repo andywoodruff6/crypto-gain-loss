b4b39951877dd0a4c9cc85ffcfc080c0bc8d5373
TheNewBadger <83123367+andywoodruff6@users.noreply.github.com>
2024-01-12 16:44:07 -0600

    Added read_address_file

---
+++ src/file.rs
@@ -0,0 +1,13 @@
+use std::{fs::File, io::Read};
+
+pub fn read_address_file() -> Vec<String> {
+    let mut file = File::open("addresses.txt").expect("File not found");
+    let mut contents = String::new();
+    let mut contents_as_vec = Vec::new();
+    file.read_to_string(&mut contents).expect("Error reading file");
+
+    contents_as_vec = contents.split('\n').map(|s| s.to_string()).collect();
+    print!("Addresses: {:?}", contents_as_vec);
+    contents_as_vec
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,30 @@
-// use blockfrost::{BlockfrostAPI, BlockfrostResult};
-
-// fn build_api() -> BlockfrostResult<BlockfrostAPI> {
-//     let api = BlockfrostAPI::new(
-//         "mainnetxvMK4xOpp5mHJgihi055KDLU64JJv2be",
-//         Default::default(),
-//     );
-
-//     Ok(api)
-// }
-
-// #[tokio::main]
-// async fn main() -> BlockfrostResult<()> {
-//     let api = build_api()?;
-//     let genesis = api.genesis().await?;
-
-//     println!("{:#?}", genesis);
-//     Ok(())
-// }
-
+use blockfrost::{BlockfrostAPI, BlockfrostResult};
 use dotenv::dotenv;
 
-fn main() {
+pub mod file;
+use crate::file::read_address_file;
+
+
+#[tokio::main]
+async fn main() -> BlockfrostResult<()> {
     dotenv().ok();
 
-    let api_key = std::env::var("BLOCKFROST_API_KEY").expect("Blockfrost API key not found");
+    let api_key: String =
+        std::env::var("BLOCKFROST_API_KEY").expect("Blockfrost API key not found");
+    let api: BlockfrostAPI = build_api(&api_key)?;
 
-    println!("API key: {}", api_key);
+    let health = api.health().await;
+    print!("Health: {:#?}", health);
+
+    let contents = read_address_file();
+
+    // print!("Addresses: {}", contents);
+
+
+    Ok(())
+}
+
+fn build_api(api_key: &str) -> BlockfrostResult<BlockfrostAPI> {
+    let api = BlockfrostAPI::new(api_key, Default::default());
+    Ok(api)
 }
