6fb48036bb838dd06fdbe4733037049c714d1421
TheNewBadger <83123367+andywoodruff6@users.noreply.github.com>
2024-01-12 13:36:20 -0600

    Added dotenv and set up api key secret

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+// use blockfrost::{BlockfrostAPI, BlockfrostResult};
+
+// fn build_api() -> BlockfrostResult<BlockfrostAPI> {
+//     let api = BlockfrostAPI::new(
+//         "mainnetxvMK4xOpp5mHJgihi055KDLU64JJv2be",
+//         Default::default(),
+//     );
+
+//     Ok(api)
+// }
+
+// #[tokio::main]
+// async fn main() -> BlockfrostResult<()> {
+//     let api = build_api()?;
+//     let genesis = api.genesis().await?;
+
+//     println!("{:#?}", genesis);
+//     Ok(())
+// }
+
+use dotenv::dotenv;
+
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+
+    let api_key = std::env::var("BLOCKFROST_API_KEY").expect("Blockfrost API key not found");
+
+    println!("API key: {}", api_key);
 }
